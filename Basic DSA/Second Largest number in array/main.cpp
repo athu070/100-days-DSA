b1f0fc4a14623021059ab32fd3989392f5fdcbcf
Atharva Sonawane <atharvasonawane34@gmail.com>
2023-09-12 14:40:02 +0530

    nothing
    nothing

---
+++ Basic DSA/Second Largest number in array/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+
+    cout<<"enter the no: ";
+    cin>>n;
+
+    for(int i=1;i<=n;i++){
+        if(n%i==0){
+            cout<<i<<endl;
+        }
+    }
+
+    return 0;
+}
