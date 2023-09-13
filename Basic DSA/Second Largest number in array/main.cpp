0fda07975a929d18323779be0aa26b25c21aea8f
Atharva Sonawane <atharvasonawane34@gmail.com>
2023-09-13 23:16:55 +0530

    nope

--- Basic DSA/Second Largest number in array/main.cpp
+++ Basic DSA/Second Largest number in array/main.cpp
@@ -3,16 +3,7 @@
 using namespace std;
 
 int main(){
-    int n;
-
-    cout<<"enter the no: ";
-    cin>>n;
-
-    for(int i=1;i<=n;i++){
-        if(n%i==0){
-            cout<<i<<endl;
-        }
-    }
+    
 
     return 0;
 }
