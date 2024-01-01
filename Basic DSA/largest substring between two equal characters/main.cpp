c4b80a9cb828c0968fa7026b705ebd710a367fbc
Atharva Sonawane <atharvasonawane34@gmail.com>
2024-01-01 23:33:30 +0530

    leetcode daily

---
+++ Basic DSA/largest substring between two equal characters/main.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#include<iostream>
+
+using namespace std;
+
+class solution{
+    public:
+    int maxLengthBetweenEqualCharacters(string s){
+        int maxDistance = -1;
+
+        for(int left=0;left < s.size(); left++){
+            for(int right = left + 1; right < s.size(); right++){
+                if(s[left]==s[right]){
+                    maxDistance = max(maxDistance, right - left - 1);
+                }
+            }
+        }
+        return maxDistance;
+    }
+};
